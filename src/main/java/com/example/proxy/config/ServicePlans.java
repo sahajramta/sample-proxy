fa4b4d5b9e123052ab8c379d823fc66ec5652d56
ashish chauhan <ashish.singh.chauhan@gmail.com>
2021-03-08 23:57:33 +0530

    updated catalog

--- src/main/java/com/example/proxy/config/ServicePlans.java
+++ src/main/java/com/example/proxy/config/ServicePlans.java
@@ -1,8 +1,6 @@
 package com.example.proxy.config;
 
-import org.springframework.cloud.servicebroker.model.catalog.Catalog;
-import org.springframework.cloud.servicebroker.model.catalog.Plan;
-import org.springframework.cloud.servicebroker.model.catalog.ServiceDefinition;
+import org.springframework.cloud.servicebroker.model.catalog.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +14,19 @@ public class ServicePlans {
                 .name("sample-free-plan")
                 .description("Sample Service Free Plan")
                 .free(true)
+                .metadata("serviceInstanceName", "sample-service-instance-1")
+                .schemas(Schemas
+                        .builder()
+                        .serviceInstanceSchema(ServiceInstanceSchema
+                                .builder()
+                                .createMethodSchema(MethodSchema
+                                        .builder()
+                                        .parameters("$schema", "http://json-schema.org/draft-04/schema#")
+                                        .parameters("type", "object")
+                                        .parameters("", "")
+                                        .build())
+                                .build())
+                        .build())
                 .build();
 
         ServiceDefinition serviceDefinition = ServiceDefinition.builder()
@@ -25,6 +36,9 @@ public class ServicePlans {
                 .bindable(true)
                 .tags("sample", "service")
                 .plans(sampleFreePlan)
+                .metadata("displayName", "sample-broker")
+                .metadata("longDescription", "A OSB broker interacting with operator service")
+                .metadata("providerDisplayName", "Sample Broker")
                 .build();
 
         return Catalog.builder()
